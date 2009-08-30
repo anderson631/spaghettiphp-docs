7b94c2695a5f52090848647ea20d0330322790a6
juliogreff <juliogreff@gmail.com>
2009-08-30 10:56:41 -0300

    added schema

---
+++ schema.js
@@ -0,0 +1,28 @@
+{
+    "por_que": "Por que o Spaghetti*?",
+    "introducao_sobre_mvc": "Introdução sobre MVC",
+    "configurando_sua_aplicacao": {
+        "index": "Configuração",
+        "rotas": "Rotas"
+    },
+    "models": {
+        "index": "Models",
+        "relacionamentos_entre_models": "Relacionamentos",
+        "condicoes_complexas": "Condições Complexas"
+    },
+    "controllers": "Controllers",
+    "views": {
+        "index": "Views",
+        "layouts": "Layouts",
+        "elements": "Elements"
+    },
+    "helpers": {
+        "index": "Helpers",
+        "html_helper": "HtmlHelper",
+        "form_helper": "FormHelper"
+    },
+    "components": {
+        "index": "Components",
+        "auth_component": "AuthComponent"
+    }
+}
